37a452e83601f6230c53dbfc093daeead3b1709e
LAPTOP-D77I3KMJ\toman <tomanom@gmail.com>
2018-10-14 20:43:37 +0300

    store

--- index.php
+++ index.php
@@ -1,2 +1,76 @@
 <?php include 'header.php';?>
+<div class="container-store">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="left-button">
+                <a href="#" class="button">STORE</a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="right-button">
+                <a href="#" class="button-center">Check out our product range</a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="right-button">
+                <a href="#" class="button">STORE</a>
+            </div>
+        </div>
+</div>
+<div class="container-buteliuku">
+    <div class="row">
+        <div class="col-md-12"
+            <div class="buteliuku-foto-wrapper">
+                <div class="buteliuku-foto">
+                    <div class="buteliuku-img">
+                        <img src="http://localhost/biolatte%20img/buteliukas.png">
+                    </div>
+                    <div class="uzrasas">
+                        <p class="pavadinimas">BIOLATTE ORIGINAL 110 KAPSILT</p>
+                        <p class="kaina1">33,60&euro;</p>
+                        <p class="kaina2">26,88&euro;</p>
+                        <a href="#" class="button-cart">ADD TO CART</a><br><br>
+                    </div>
+                </div>
+
+                <div class="buteliuku-foto">
+                    <div class="buteliuku-img">
+                        <img src="http://localhost/biolatte%20img/buteliukas.png">
+                    </div>
+                    <div class="uzrasas">
+                        <p class="pavadinimas">BIOLATTE ORIGINAL 110 KAPSILT</p>
+                        <p class="kaina1">33,60&euro;</p>
+                        <p class="kaina2">26,88&euro;</p>
+                        <a href="#" class="button-cart">ADD TO CART</a><br><br>
+                    </div>
+                </div>
+
+                <div class="buteliuku-foto">
+                    <div class="buteliuku-img">
+                        <img src="http://localhost/biolatte%20img/buteliukas.png">
+                    </div>
+                    <div class="uzrasas">
+                        <p class="pavadinimas">BIOLATTE ORIGINAL 110 KAPSILT</p>
+                        <p class="kaina1">33,60&euro;</p>
+                        <p class="kaina2">26,88&euro;</p>
+                        <a href="#" class="button-cart">ADD TO CART</a><br><br>
+                    </div>
+                </div>
+
+                <div class="buteliuku-foto">
+                    <div class="buteliuku-img">
+                        <img src="http://localhost/biolatte%20img/buteliukas.png">
+                    </div>
+                    <div class="uzrasas">
+                        <p class="pavadinimas">BIOLATTE ORIGINAL 110 KAPSILT</p>
+                        <p class="kaina1">33,60&euro;</p>
+                        <p class="kaina2">26,88&euro;</p>
+                        <a href="#" class="button-cart">ADD TO CART</a><br><br>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
 <?php include 'footer.php';?>
