16f0a7842048d177ccd9ab175b745144238497f6
LAPTOP-D77I3KMJ\toman <tomanom@gmail.com>
2018-10-16 23:57:22 +0300

    iki deliver

--- index.php
+++ index.php
@@ -106,7 +106,8 @@
     </div>
 </div>
 </div>
-<div class="container">
+<div class="container-wrapper">
+<div class="container-zmoniu">
     <div class="row">
         <div class="col-md-12">
             <div class="zmoniu-foto-wrapper">
@@ -117,6 +118,110 @@
                     <div class="zmoniu-uzrasai">
                         <h4>Pavadinimas</h4>
                     </div>
+                </div>
+                <div class="zmoniu-foto">
+                    <div class="zmoniu-img">
+                        <img src="http://localhost/biolatte%20img/zmones1.png">
+                    </div>
+                    <div class="zmoniu-uzrasai">
+                        <h4>Pavadinimas</h4>
+                    </div>
+                </div>
+                <div class="zmoniu-foto">
+                    <div class="zmoniu-img">
+                        <img src="http://localhost/biolatte%20img/zmones1.png">
+                    </div>
+                    <div class="zmoniu-uzrasai">
+                        <h4>Pavadinimas</h4>
+                    </div>
+                </div>
+
+                <div class="zmoniu-foto">
+                    <div class="zmoniu-img">
+                        <img src="http://localhost/biolatte%20img/zmones1.png">
+                    </div>
+                    <div class="zmoniu-uzrasai">
+                        <h4>Pavadinimas</h4>
+                    </div>
+                </div>
+
+                <div class="zmoniu-foto">
+                    <div class="zmoniu-img">
+                        <img src="http://localhost/biolatte%20img/zmones1.png">
+                    </div>
+                    <div class="zmoniu-uzrasai">
+                        <h4>Pavadinimas</h4>
+                    </div>
+                </div>
+
+                <div class="zmoniu-foto">
+                    <div class="zmoniu-img">
+                        <img src="http://localhost/biolatte%20img/zmones1.png">
+                    </div>
+                    <div class="zmoniu-uzrasai">
+                        <h4>Pavadinimas</h4>
+                    </div>
+                </div>
+
+                <div class="zmoniu-foto">
+                    <div class="zmoniu-img">
+                        <img src="http://localhost/biolatte%20img/zmones1.png">
+                    </div>
+                    <div class="zmoniu-uzrasai">
+                        <h4>Pavadinimas</h4>
+                    </div>
+                </div>
+
+                <div class="zmoniu-foto">
+                    <div class="zmoniu-img">
+                        <img src="http://localhost/biolatte%20img/zmones1.png">
+                    </div>
+                    <div class="zmoniu-uzrasai">
+                        <h4>Pavadinimas</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<div class="container-obuolys">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="obuolys-uzrasai-wrapper">
+                <div class="uzrasai">
+                    <h3>Lorem <br>ipsum dolor<br> sit amet</h3>
+                    <p>In bibendum at metus nec aliquam</p>
+                    <p class="mygtukas">Duis gravida, metus a ullamcorper!</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-delivery">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="delivery-uzrasas-wrapper">
+                    <img src="http://localhost/biolatte%20img/masina.png">
+                    <h4>SODNE TRANSPORT</h4>
+                    <P>SmartPost, Post24, DPD</P>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="delivery-uzrasas-wrapper">
+                    <img src="http://localhost/biolatte%20img/etikete.png">
+                    <h4>HINNAGARANTIE</h4>
+                    <P>SmartPost, Post24, DPD</P>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="delivery-uzrasas-wrapper">
+                    <img src="http://localhost/biolatte%20img/krepselis-rodykle.png">
+                    <h4>SODNE TRANSPORT</h4>
+                    <P>SmartPost, Post24, DPD</P>
                 </div>
             </div>
         </div>
